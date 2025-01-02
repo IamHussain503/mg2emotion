d7bd67eba9b1a32c5091e58ce8b4b02f9f502240
IamHussain <33412791+IamHussain503@users.noreply.github.com>
2025-01-02 20:01:46 +0500

    Update clm_embeddings.py

--- clm_embeddings.py
+++ clm_embeddings.py
@@ -167,7 +167,7 @@ def main():
     emotion_tokenizer = AutoTokenizer.from_pretrained("nateraw/bert-base-uncased-emotion")
     emotion_model = AutoModelForSequenceClassification.from_pretrained("nateraw/bert-base-uncased-emotion").to(device)
 
-    model = CLaMP(clap_model, embedding_dim=256).to(device)
+    model = CLaMP(clap_model, embedding_dim=512).to(device)
 
     # Load checkpoint and remove unexpected keys
     state_dict = torch.load(args.checkpoint, map_location=device)
@@ -210,7 +210,7 @@ def main():
             print(f'melody_emb shape::::::::::::::::::::::::::::::::::::::::::::::::::::::::', melody_emb.shape)
             text_emb = embeddings[:, 2*embedding_dim:3*embedding_dim].cpu().numpy()
             print(f'text_emb shape::::::::::::::::::::::::::::::::::::::::::::::::::::::::', text_emb.shape)
-            emotion_emb = embeddings[:, 3*embedding_dim:].cpu().numpy()
+            emotion_emb = embeddings[:, 3*embedding_dim:3*embedding_dim+embedding_dim].cpu().numpy()
             print(f'emotion_emb shape::::::::::::::::::::::::::::::::::::::::::::::::::::::::', emotion_emb.shape) 
 
 
